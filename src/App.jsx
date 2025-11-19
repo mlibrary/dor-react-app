55d9faebddd161ccd6a499633af74a2a96330ee3
Greg Kostin <gkostin@umich.edu>
2025-11-19 13:47:55 -0500

    dor-dc

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import FlightSearchApp from './apps/FlightSearchApp/index.jsx';
 import RsFlightSearchApp from './apps/RsFlightSearchApp/index.jsx';
 import RsApiFlightSearchApp from './apps/RsApiFlightSearchApp/index.jsx';
 import RsBhlApp from './apps/RsBhlApp/index.jsx';
+import RsDorDcApp from './apps/RsDorDcApp/index.jsx';
 
 function App() {
     return (
         <BrowserRouter>
             <nav style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
+                <Link to="/dor-dc" style={{ marginRight: '20px' }}>RS-DOR-DC</Link>
                 <Link to="/" style={{ marginRight: '20px' }}>Open Search API</Link>
                 <Link to="/rs-api" style={{ marginRight: '20px' }}>Reactive Search API</Link>
                 <Link to="/rs" style={{ marginRight: '20px' }}>Reactive Search Components</Link>
@@ -16,6 +18,7 @@ function App() {
             </nav>
 
             <Routes>
+                <Route path="/dor-dc" element={<RsDorDcApp />} />
                 <Route path="/" element={<FlightSearchApp />} />
                 <Route path="/rs-api" element={<RsApiFlightSearchApp />} />
                 <Route path="/rs" element={<RsFlightSearchApp />} />
---
+++ src/apps/RsDorDcApp/index.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+// console.log(Object.keys(ReactiveSearch));
+import {
+    ReactiveBase,
+    SearchBox,
+    MultiList,
+    SelectedFilters,
+    ReactiveList,
+    ResultCard,
+    MultiRange,
+    RangeInput,
+    RangeSlider,
+    DynamicRangeSlider,
+} from '@appbaseio/reactivesearch';
+
+import {
+    Row,
+    Button,
+    Col,
+    Card,
+} from 'antd';
+
+import createDOMPurify from 'dompurify';
+
+const DOMPurify = createDOMPurify(window);
+
+function RsDorDcApp() {
+    return (
+        <div style={{ padding: '20px', maxWidth: '100%', margin: '0 auto' }}>
+            <ReactiveBase
+                app="dor-dc"
+                credentials="rs-admin-user:rs-password"
+                url="http://localhost:8000"
+                reactivesearchAPIConfig={{
+                    recordAnalytics: false,
+                    suggestionAnalytics: false,
+                    enableQueryRules: false,
+                }}
+            >
+                <Row gutter={16} style={{ padding: 20 }}>
+                    <Col span={6}>
+                        <Card>
+                            <MultiList
+                                componentId="collectionfilter"
+                                dataField="collection_name"
+                                // size={100}
+                                // style={{
+                                //     marginBottom: 20
+                                // }}
+                                title="Collection"
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={18}>
+                        <SearchBox
+                            // autosuggest={false}
+                            componentId="search"
+                            dataField={["all"]}
+                            placeholder="Search All"
+                        />
+                        <SelectedFilters />
+                        <div id="result">
+                            <ReactiveList
+                                componentId="results"
+                                dataField="all"
+                                size={9}
+                                pagination={true}
+                                react={{
+                                    and: ["search", "collectionfilter"],
+                                }}
+                                render={({ data }) => (
+                                    <ReactiveList.ResultCardsWrapper>
+                                        {data.map((item) => (
+                                            <ResultCard key={item._id}>
+                                                <ResultCard.Title
+                                                    dangerouslySetInnerHTML={{
+                                                        __html: item.collection_name
+                                                    }}
+                                                />
+                                                <ResultCard.Description>
+                                                    <div>
+                                                        {/*<div>{item.subjects}</div>*/}
+                                                        {/*<div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(item.description || '') }} />*/}
+                                                        {/*<br/>*/}
+                                                        <div>{item.all}</div>
+                                                        {/*<div>Collection: {item.collection_title}</div>*/}
+                                                        {/*<div>ID: {item.id}</div>*/}
+                                                        {/*<div>EAD: {item.finding_aid}</div>*/}
+                                                    </div>
+                                                </ResultCard.Description>
+                                            </ResultCard>
+                                        ))}
+                                    </ReactiveList.ResultCardsWrapper>
+                                )}
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            </ReactiveBase>
+        </div>
+    );
+}
+
+export default RsDorDcApp;
