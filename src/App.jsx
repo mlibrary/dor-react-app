5f40fe836ea7a7c8595caa2cb1ef7f6dbdf7b374
Greg Kostin <gkostin@umich.edu>
2025-12-04 10:37:51 -0500

    Fix App.jsx link to Link

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <BrowserRouter>
             <nav style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
                 <Link to="/" style={{ marginRight: '20px' }}>RS-DOR-DC</Link>
-                <link to="/os" style={{marginRight: '20px' }}>OS-DOR-DC</link>
+                <Link to="/os" style={{marginRight: '20px' }}>OS-DOR-DC</Link>
                 <Link to="/flight" style={{ marginRight: '20px' }}>Open Search API</Link>
                 <Link to="/rs-api" style={{ marginRight: '20px' }}>Reactive Search API</Link>
                 <Link to="/rs" style={{ marginRight: '20px' }}>Reactive Search Components</Link>
