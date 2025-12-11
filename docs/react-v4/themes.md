474b65d5adc36ab75deb78b639d204c2e0c65a38
Greg Kostin <gkostin@umich.edu>
2025-12-11 09:16:58 -0500

    Remove Examples

--- docs/react-v4/themes.md
+++
@@ -1,6 +0,0 @@
-h
-https://docs.reactivesearch.io/docs/reactivesearch/react/theming/overview/
-https://docs.reactivesearch.io/docs/reactivesearch/react/theming/style/
-https://docs.reactivesearch.io/docs/reactivesearch/react/theming/classnameinjection/
-
-
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import FlightSearchApp from './apps/FlightSearchApp/index.jsx';
-// import RsFlightSearchApp from './apps/RsFlightSearchApp/index.jsx';
-// import RsApiFlightSearchApp from './apps/RsApiFlightSearchApp/index.jsx';
-// import RsBhlApp from './apps/RsBhlApp/index.jsx';
 import RsDorDcApp from './apps/RsDorDcApp/index.jsx';
 import OsDorDcApp from './apps/OsDorDcApp/index.jsx';
 
@@ -13,19 +9,11 @@ function App() {
             <nav style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
                 <Link to="/" style={{ marginRight: '20px' }}>RS-DOR-DC</Link>
                 <Link to="/os" style={{marginRight: '20px' }}>OS-DOR-DC</Link>
-                {/*<Link to="/flight" style={{ marginRight: '20px' }}>Open Search API</Link>*/}
-                {/*<Link to="/rs-api" style={{ marginRight: '20px' }}>Reactive Search API</Link>*/}
-                {/*<Link to="/rs" style={{ marginRight: '20px' }}>Reactive Search Components</Link>*/}
-                {/*<Link to="/bhl" style={{ marginRight: '20px' }}>RS-BHL</Link>*/}
             </nav>
 
             <Routes>
                 <Route path="/" element={<RsDorDcApp />} />
                 <Route path="/os" element={<OsDorDcApp />} />
-                {/*<Route path="/flight" element={<FlightSearchApp />} />*/}
-                {/*<Route path="/rs-api" element={<RsApiFlightSearchApp />} />*/}
-                {/*<Route path="/rs" element={<RsFlightSearchApp />} />*/}
-                {/*<Route path="/bhl" element={<RsBhlApp />} />*/}
             </Routes>
         </BrowserRouter>
     );
