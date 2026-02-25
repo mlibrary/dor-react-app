26137e63734c7d2ca9f458afa31c65a49ca1fd82
Greg Kostin <gkostin@umich.edu>
2026-02-25 16:12:52 -0500

    change to single app for now

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import RsDorDcApp from './apps/RsDorDcApp/index.jsx';
 import OsDorDcApp from './apps/OsDorDcApp/index.jsx';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <nav style={{padding: '20px', backgroundColor: '#f0f0f0'}}>
-                <Link to="/" style={{marginRight: '20px'}}>RS-DOR-DC</Link>
-                <Link to="/os" style={{marginRight: '20px'}}>OS-DOR-DC</Link>
-            </nav>
+    return <RsDorDcApp />;
 
-            <Routes>
-                <Route path="/" element={<RsDorDcApp/>}/>
-                <Route path="/os" element={<OsDorDcApp/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+    // return (
+    //     <BrowserRouter>
+    //         <nav style={{padding: '20px', backgroundColor: '#f0f0f0'}}>
+    //             <Link to="/" style={{marginRight: '20px'}}>RS-DOR-DC</Link>
+    //             <Link to="/os" style={{marginRight: '20px'}}>OS-DOR-DC</Link>
+    //         </nav>
+    //
+    //         <Routes>
+    //             <Route path="/" element={<RsDorDcApp/>}/>
+    //             <Route path="/os" element={<OsDorDcApp/>}/>
+    //         </Routes>
+    //     </BrowserRouter>
+    // );
 }
 
 export default App;
